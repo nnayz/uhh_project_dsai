52c66c9a07cc075028ee77d231fe7a226f97778d
nnayz <nasrul2001@gmail.com>
2025-11-29 15:54:37 +0100

    cli setup

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Intentionally left blank (2)
+# Intentionally left blank
+import click
+
+@click.group()
+@click.version_option("1.0.0", "-v", "--version", help="Show version and exit.")
+def cli():
+    """
+    CLI for the project [Easy Access]
+    """
+    pass
