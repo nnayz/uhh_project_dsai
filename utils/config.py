c70f235cd56be4fd012a3cfdb89dd9ad371064b4
Nasrul Huda <hi@nasrul.info>
2025-12-10 16:39:44 +0100

    Prototypical Network Documentation (#3)
    * dependencies, util helper files and config class
    
    * Doc notes for protypical network
    
    * Baseline architecture
    
    * Metadata Information

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    SAMPLING_RATE: int = 16000
+    DATA_DIR: Path = Path("/data/msc-proj/")
+    RANDOM_SEED: int = 42
