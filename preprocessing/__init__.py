a9104c82150a59ec01f00cb17b8d1d439afa0439
Nasrul Huda <hi@nasrul.info>
2026-01-18 19:57:12 +0100

    val eval for viz (#47)
    * event detection viz and v1 results on pcen features, neg csv contrast, monitor as fmeasure
    
    * val_eval

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,11 +1,19 @@
-from .ann_service import AnnotationService
+from .ann_service import (
+    parse_positive_events_train,
+    parse_negative_events_train,
+    parse_positive_events_val,
+    parse_negative_events_val,
+)
 from .preprocess import load_audio, waveform_to_logmel, extract_logmel_segment
 from .datamodule import DCASEFewShotDataModule, create_datamodule
 from .feature_export import export_features, validate_features
 
 __all__ = [
-    # Annotation service
-    "AnnotationService",
+    # Annotation service functions
+    "parse_positive_events_train",
+    "parse_negative_events_train",
+    "parse_positive_events_val",
+    "parse_negative_events_val",
     # Audio preprocessing
     "load_audio",
     "waveform_to_logmel",
