054ccbd45e6a7cebe85b2907d80c2c263379b766
nnayz <nasrul2001@gmail.com>
2026-01-21 14:37:44 +0100

    postprocessing refactor

--- archs/v2/lightning_module.py
+++ archs/v2/lightning_module.py
@@ -281,8 +281,8 @@ class ProtoNetV2LightningModule(L.LightningModule):
     def _run_val_event_eval(self) -> None:
         from preprocessing.sequence_data import PrototypeAdaSeglenBetterNegTestSetV2
         from utils.evaluation import evaluate as eval_fn
-        from utils.post_proc import post_processing as post_proc
-        from utils.post_proc_new import post_processing as post_proc_new
+        from postprocessing.post_proc import post_processing as post_proc
+        from postprocessing.post_proc_new import post_processing as post_proc_new
 
         self.onset_offset = {}
 
---
+++ postprocessing/__init__.py
@@ -0,0 +1,8 @@
+from .post_proc import post_processing, post_processing_remove_short_negative
+from .post_proc_new import post_processing as post_processing_new
+
+__all__ = [
+    "post_processing",
+    "post_processing_remove_short_negative",
+    "post_processing_new",
+]
